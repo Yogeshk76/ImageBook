9f5142df7d1debb24de5de71a13f2280cc7098cd
Yogeshk76 <yogeshkumar60508@gmail.com>
2025-07-14 17:48:47 +0530

    feat: implement AuthService for user authentication with Appwrite

---
+++ src/appwrite/auth.js
@@ -0,0 +1,71 @@
+import conf from '../conf.js';
+import { Client, Account, ID} from 'appwrite';
+
+export class AuthService {
+  client = new Client();
+  account;
+
+// Initialize Appwrite client and account only when AuthService is instantiated.
+// This ensures resources are set up on-demand, improving efficiency and encapsulation.
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+
+    this.account = new Account(this.client);
+  }
+
+
+  async createAccount({email, password, name}) {
+    try {
+      const userAccount = await this.account.create(ID.unique(), email, password, name);
+
+      if (userAccount) {
+        //call another method to log in user if account is created successfully
+
+        return this.login({email, password});
+      }
+      else {
+        return userAccount;
+      }
+
+      return userAccount;
+    } catch (error) {
+      console.error('Error creating account:', error);
+      throw error;
+    }
+  }
+
+  async login({email, password}) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      console.error('Error logging in:', error);
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.error('Error getting current user:', error);
+    }
+
+    return null;
+  }
+
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+
+    return null;
+  }
+}
+const authService = new AuthService();
+
+export default authService;
