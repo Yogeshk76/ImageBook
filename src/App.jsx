adcb98f7973b1290e731e68024e2b727a6cd3a35
Yogeshk76 <yogeshkumar60508@gmail.com>
2025-07-15 11:25:37 +0530

    feat: implement layout structure with Header and Footer components; integrate user authentication in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
+import React, {useState, useEffect, use} from 'react'
+import './App.css';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import {login, logout} from './store/authSlice';
+import {Header} from './components'
+import {Footer} from './components'
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData) {
+        dispatch(login({userData}))
+      }else {
+        dispatch(logout())
+      }
+    })
+    .catch((error) => {
+      console.error('Error fetching user data:', error);
+    })
+    .finally(() => {
+      setLoading(false);
+    })
+  }, []);
+
+    if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    );
+  }
+
   return (
-    <>
-      <h1 className="text-4xl font-bold">Welcome to My App</h1>
-    </>
-  )
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO : <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default App
--- src/appwrite/upload.js
+++
@@ -1,2 +0,0 @@
-import conf from './conf.js';
-import { Appwrite } from 'appwrite';
