35cb1c7d2983ab3a3d0437a8f12b7d572d596472
Yogeshk76 <yogeshkumar60508@gmail.com>
2025-07-15 17:14:23 +0530

    feat: add Button and Input components; update Header to include Logout button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -60,6 +60,11 @@ const Header = () => {
                 </li>
               ) : null
             )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
           </ul>
         </nav>
       </Container>
