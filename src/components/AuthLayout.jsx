61580872396948ca4e1a40fc70433418291db886
Yogeshk76 <yogeshkumar60508@gmail.com>
2025-07-16 12:32:57 +0530

    feat: enhance loading state in Protected component with spinner; add Rich Text Editor component

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -18,6 +18,8 @@ export default function Protected({children, authentication = true}){
 
     setLoader(false);
   }, [authStatus, navigate, authentication]);
-  return loader ? <h1>Loading....</h1> : <>{children}</>;
+  return loader ? <div className="flex items-center justify-center h-screen">
+        <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+      </div> : <>{children}</>;
 }
 
